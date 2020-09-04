b611cce4ccd4db1fb23981c8021cacec645a3558
Nikollas <nikollasferreirag@gmail.com>
2020-09-04 15:16:41 -0300

    feat: cria função de teste da API

--- app/Http/Controllers/Api/SalaoController.php
+++ app/Http/Controllers/Api/SalaoController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Salao;
 use App\Models\Util;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
@@ -110,5 +111,24 @@ class SalaoController extends Controller {
             return response()->json(['Ok'], 200);
         }
         return response()->json(['Erro'], 400);
+    }
+
+    public function teste(Request $request) {
+        try {
+            Validator::make($request->only(['latitude', 'longitude', 'cidade']), [
+                'latitude' => 'required',
+                'longitude' => 'required',
+                'cidade' => 'required|string',
+            ])->validate();
+            $latitude = $request->latitude;
+            $longitude = $request->longitude;
+            $cidade = $request->cidade;
+            $salao = DB::select("SELECT *,
+                                        haversine(saloes_view.latitude,saloes_view.longitude,$latitude,$longitude)
+                                        AS distancia FROM saloes_view where cidade = '$cidade' ORDER BY distancia,nome");
+            return response()->json($salao, 200);
+        } catch (ValidationException $e) {
+            return response()->json($e, 400);
+        }
     }
 }
