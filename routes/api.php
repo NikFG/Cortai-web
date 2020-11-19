a748ab7eef5a9c03d696136b3c6253963d5aea10
Nikollas <nikollasferreirag@gmail.com>
2020-11-19 08:35:11 -0300

    feat: cria grupo rotas horario

--- routes/api.php
+++ routes/api.php
@@ -4,32 +4,48 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', 'Api\AuthController@login')->name('login');
+    Route::post('login/google', 'Api\AuthController@loginGoogle');
+    Route::post('user/create', 'UserController@store');
+});
 
-Route::post('auth/login', 'Api\AuthController@login')->name('login');
-Route::post('auth/login/google', 'Api\AuthController@loginGoogle');
-Route::post('auth/user/create', 'UserController@store');
 
 Route::group(['middleware' => ['apiJwt']], function () {
     Route::post('auth/logout', 'Api\AuthController@logout');
     Route::get('users', 'UserController@index');
 
     //Salão
-    Route::get('saloes/home','Api\SalaoController@home');
-    Route::post('saloes/store', 'Api\SalaoController@store');
-    Route::post('saloes/edit/{id}', 'Api\SalaoController@update');
-    Route::get('saloes/show/{id}', 'Api\SalaoController@show');
-    Route::delete('saloes/destroy/{id}','Api\SalaoController@destroy');
-    Route::patch('saloes/restore/{id}','Api\SalaoController@restore');
+    Route::group(['prefix' => 'saloes'], function () {
+        Route::get('home', 'Api\SalaoController@home');
+        Route::post('store', 'Api\SalaoController@store');
+        Route::post('edit/{id}', 'Api\SalaoController@update');
+        Route::get('show/{id}', 'Api\SalaoController@show');
+        Route::delete('destroy/{id}', 'Api\SalaoController@destroy');
+        Route::patch('restore/{id}', 'Api\SalaoController@restore');
+    });
 
 
     //Serviço
-    Route::get('servicos', 'Api\ServicoController@index');
-    Route::post('servicos/store', 'Api\ServicoController@store');
-    Route::post('servicos/edit/{id}', 'Api\ServicoController@update');
-    Route::get('servicos/show/{id}', 'Api\ServicoController@show');
-    Route::delete('servicos/destroy/{id}','Api\ServicoController@destroy');
-    Route::patch('servicos/restore/{id}','Api\ServicoController@restore');
-    
+    Route::group(['prefix' => 'servicos'], function () {
+        Route::get('/', 'Api\ServicoController@index');
+        Route::post('store', 'Api\ServicoController@store');
+        Route::post('edit/{id}', 'Api\ServicoController@update');
+        Route::get('show/{id}', 'Api\ServicoController@show');
+        Route::delete('destroy/{id}', 'Api\ServicoController@destroy');
+        Route::patch('restore/{id}', 'Api\ServicoController@restore');
+    });
+    //Horário
+    Route::group(['prefix' => 'horarios'], function () {
+        Route::get('/cliente', 'Api\HorarioController@clienteIndex');
+        Route::get('/cabeleireiro', 'Api\HorarioController@cabeleireiroIndex');
+        Route::post('store', 'Api\HorarioController@store');
+        Route::post('edit/{id}', 'Api\HorarioController@update');
+        Route::get('show/{id}', 'Api\HorarioController@show');
+        Route::delete('destroy/{id}', 'Api\HorarioController@destroy');
+    });
+
+
 });
 
 
