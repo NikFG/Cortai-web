b86da165b0447fc651bc6c8d93beab84de662614
Nikollas <nikollasferreirag@gmail.com>
2020-11-30 11:13:30 -0300

    feat: Modifica canal para autenticação

--- routes/channels.php
+++ routes/channels.php
@@ -12,9 +12,15 @@ use Illuminate\Support\Facades\Broadcast;
 | used to check if an authenticated user can listen to the channel.
 |
 */
+//Broadcast::routes(['middleware' => ['auth:api']]);
+Broadcast::routes(['middleware' => ['apiJwt']]);
 
-Broadcast::routes(['middleware' => 'apiJwt']);
-Broadcast::channel('user.{id}', function ($user, $id) {
-    return true;
+
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
 });
+//
+//Broadcast::channel('user.{id}', function ($user, $id) {
+//    return true;
+//});
 
