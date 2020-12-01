d3cec7503c9577a23436bd0efa0776ccca420990
Nikollas <nikollasferreirag@gmail.com>
2020-12-01 11:06:31 -0300

    feat: cria função teste para testar com o pusher

--- app/Http/Controllers/Api/AgendaController.php
+++ app/Http/Controllers/Api/AgendaController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Events\AgendaCabeleireiro;
 use App\Events\ContaConfirmar;
 use App\Events\ControlaAgenda;
 use App\Http\Controllers\Controller;
 use App\Models\Horario;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AgendaController extends Controller {
 
@@ -18,9 +20,10 @@ class AgendaController extends Controller {
     }
 
     public function teste() {
-        $quantidade = Horario::where('confirmado', false)->count();
-        event(new ContaConfirmar(22, $quantidade));
-        return response()->json($quantidade);
+        $horario = Horario::where('pago',false)->get();
+        $u = Auth::user();
+        event(new AgendaCabeleireiro($horario, 22));
+        return response()->json($horario);
     }
 
 
