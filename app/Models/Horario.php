3415ff0bbe77ef569e29fe885ddc6a310ec3c7c1
Nikollas <nikollasferreirag@gmail.com>
2020-12-01 11:06:42 -0300

    feat: cria cast de data e hora em horarios

--- app/Models/Horario.php
+++ app/Models/Horario.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Horario extends Model {
 
+    /**
+     * @var mixed
+     */
+
+    protected $casts = [
+        'data' => 'date:d/m/Y',
+        'hora' => 'date:H:i'
+    ];
+
     public function avaliacoes() {
         return $this->hasMany('App\Models\Avaliacao');
     }
@@ -25,6 +34,7 @@ class Horario extends Model {
     public function forma_pagamento() {
         return $this->belongsTo('App\Models\FormaPagamento');
     }
+
     public function servicos() {
         return $this->belongsToMany('App\Models\Servico',
             'horario_servicos', 'horario_id')
