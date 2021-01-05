4f416a21346f89f0e5e8d4ad50de4a2b351b4f40
Nikollas <nikollasferreirag@gmail.com>
2021-01-05 15:34:52 -0300

    Auto stash before revert of "Merge remote-tracking branch 'origin/main'"

--- database/migrations/2020_09_04_175932_create_haversine_function.php
+++ database/migrations/2020_09_04_175932_create_haversine_function.php
@@ -11,12 +11,27 @@ class CreateHaversineFunction extends Migration {
      * @return void
      */
     public function up() {
-        DB::unprepared("CREATE OR REPLACE FUNCTION haversine(latitude1 numeric(10,6),longitude1 numeric(10,6), latitude2 numeric(10,6), longitude2 numeric(10,6))
-RETURNS double precision AS
-$BODY$
-	SELECT 6371 * acos( cos( radians(latitude1) ) * cos( radians( latitude2 ) ) * cos( radians( longitude1 ) - radians(longitude2) ) + sin( radians(latitude1) ) * sin( radians( latitude2 ) ) ) AS distance
-$BODY$
-LANGUAGE sql;");
+        DB::unprepared("CREATE FUNCTION haversine (lat1 DOUBLE, lng1 DOUBLE, lat2 DOUBLE, lng2 DOUBLE) RETURNS DECIMAL(30,15)
+                    BEGIN
+                        DECLARE R INT;
+                        DECLARE dLat DECIMAL(30,15);
+                        DECLARE dLng DECIMAL(30,15);
+                        DECLARE a1 DECIMAL(30,15);
+                        DECLARE a2 DECIMAL(30,15);
+                        DECLARE a DECIMAL(30,15);
+                        DECLARE c DECIMAL(30,15);
+                        DECLARE d DECIMAL(30,15);
+                    
+                        SET R = 6371; -- Earth's radius in miles
+                        SET dLat = RADIANS( lat2 ) - RADIANS( lat1 );
+                        SET dLng = RADIANS( lng2 ) - RADIANS( lng1 );
+                        SET a1 = SIN( dLat / 2 ) * SIN( dLat / 2 );
+                        SET a2 = SIN( dLng / 2 ) * SIN( dLng / 2 ) * COS( RADIANS( lng1 )) * COS( RADIANS( lat2 ) );
+                        SET a = a1 + a2;
+                        SET c = 2 * ATAN2( SQRT( a ), SQRT( 1 - a ) );
+                        SET d = R * c;
+                        RETURN d;
+                    END");
     }
 
     /**
