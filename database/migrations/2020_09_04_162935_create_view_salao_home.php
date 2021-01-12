5ebd061da9d79fa500a31ffd338059d19d70060e
Nikollas <nikollasferreirag@gmail.com>
2021-01-12 19:38:58 -0300

    feat: modifica salão_view

--- database/migrations/2020_09_04_162935_create_view_salao_home.php
+++ database/migrations/2020_09_04_162935_create_view_salao_home.php
@@ -11,30 +11,26 @@ class CreateViewSalaoHome extends Migration {
      * @return void
      */
     public function up() {
-        DB::statemnt("CREATE VIEW saloes_view AS 
-                    SELECT 
-                        saloes.id AS id,
-                        saloes.nome AS nome,
-                        saloes.cidade AS cidade,
-                        saloes.endereco AS endereco,
-                        saloes.imagem AS imagem,
-                        saloes.latitude AS latitude,
-                        saloes.longitude AS longitude,
-                        saloes.telefone AS telefone,
-                        saloes.created_at AS created_at,
-                        saloes.updated_at AS updated_at,
-                        COUNT(avaliacoes.id) AS qtd_avaliacao,
-                        AVG(avaliacoes.valor) AS media,
-                        MIN(servicos.valor) AS menor_valor,
-                        MAX(servicos.valor) AS maior_valor
-                    FROM
-                        (((saloes
-                        INNER JOIN horarios ON (saloes.id = horarios.salao_id))
-                        INNER JOIN servicos ON (servicos.salao_id = saloes.id))
-                        INNER JOIN avaliacoes ON (horarios.id = avaliacoes.horario_id))
-                    WHERE
-                        servicos.ativo = 1
-                    GROUP BY saloes.id");
+        DB::statement("CREATE OR REPLACE VIEW saloes_view AS
+                    select saloes.id                        AS id,
+                           saloes.nome                      AS nome,
+                           saloes.cidade                    AS cidade,
+                           saloes.endereco                  AS endereco,
+                           saloes.imagem                    AS imagem,
+                           saloes.latitude                  AS latitude,
+                           saloes.longitude                 AS longitude,
+                           saloes.telefone                  AS telefone,
+                           saloes.created_at                AS created_at,
+                           saloes.updated_at                AS updated_at,
+                           count(avaliacoes.id)             AS qtd_avaliacao,
+                           ifnull(avg(avaliacoes.valor), 0) AS media,
+                           ifnull(min(servicos.valor), 0)   AS menor_valor,
+                           ifnull(max(servicos.valor), 0)   AS maior_valor
+                    from (((saloes left join horarios on (saloes.id = horarios.salao_id)) left join servicos on (servicos.salao_id = saloes.id))
+                             left join avaliacoes on (horarios.id = avaliacoes.horario_id))
+                    where servicos.deleted_at is null
+                      and saloes.deleted_at is null
+                    group by saloes.id;");
     }
 
     /**
