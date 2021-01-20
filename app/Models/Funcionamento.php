1894015fa17a06ad49d721f397c3b6cf76e1135b
Nikollas <nikollasferreirag@gmail.com>
2021-01-20 14:13:55 -0300

    feat: cria casts para funcionamento

--- app/Models/Funcionamento.php
+++ app/Models/Funcionamento.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Funcionamento extends Model {
+    /**
+     * @var mixed
+     */
+    protected $casts = [
+        'horario_abertura' => 'date:H:i',
+        'horario_fechamento' => 'date:H:i'
+    ];
+
 
     public function salao() {
         return $this->belongsTo('App\Models\Salao');
