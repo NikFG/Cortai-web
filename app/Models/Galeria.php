be507ff72c17e45be851ee7117c9eb9238b48740
Nikollas <nikollasferreirag@gmail.com>
2021-01-21 15:59:47 -0300

    feat: melhora modelo galeria

--- app/Models/Galeria.php
+++ app/Models/Galeria.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Galeria extends Model
-{
+class Galeria extends Model {
     use HasFactory;
+
+
+    public function servico() {
+        return $this->belongsTo('App\Models\Servico');
+    }
+    public function cabeleireiro() {
+        return $this->belongsTo('App\Models\User');
+    }
+    public function cliente() {
+        return $this->belongsTo('App\Models\User');
+    }
+    public function salao(){
+        return $this->belongsTo('App\Models\Salao');
+    }
 }
