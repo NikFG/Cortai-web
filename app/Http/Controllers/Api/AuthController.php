d7d34cf580fe6aa346b3fe1dceeca51876d4e952
Nikollas <nikollasferreirag@gmail.com>
2021-04-14 15:55:28 -0300

    feat: modifica auth_controller para verificar o retorno da biblioteca

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
+use Laravel\Socialite\Facades\Socialite;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 
@@ -25,12 +27,12 @@ class AuthController extends Controller {
     }
 
 
-    protected function respondWithToken($token): \Illuminate\Http\JsonResponse {
+    private function respondWithToken($token): JsonResponse {
         $user = JWTAuth::setToken($token)->toUser();
 
         if ($user->email_verified_at != null || $user->is_google) {
             if ($user->imagem != null)
-                $user->imagem = base64_encode(Storage::cloud()->get($user->imagem));
+//                $user->imagem = base64_encode(Storage::cloud()->get($user->imagem));
             return response()->json([
                 'access_token' => $token,
                 'token_type' => 'bearer',
@@ -43,31 +45,33 @@ class AuthController extends Controller {
         return response()->json('Email não verificado, olhe sua caixa de entrada ou spam', 403);
     }
 
+    public function loginGoogle2(Request $request) {
+        $user = Socialite::driver('google')->userFromToken($request->password);
+        return response()->json(["u" => $user, "request" => $request->all()]);
+    }
+
     /* criar login google */
-    public function loginGoogle(Request $request): \Illuminate\Http\JsonResponse {
-        $credentials = $request->only(['email', 'password']);
+    public function loginGoogle(Request $request): JsonResponse {
+        $credentials = $request->only(['email', 'token']);
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
-            'nome' => 'string',
-            'password' => 'required|string',
-            'telefone' => '',
-            'imagem' => '',
+            'token' => 'required|string',
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
-
+        $google_user = Socialite::driver('google')->userFromToken($request->token);
         $user = User::where('email', $request->email)->first();
         if ($user == null) {
             $user = new User();
-            $user->nome = $request->nome;
-            $user->password = bcrypt($request->password);
-            $user->email = $request->email;
-            $user->telefone = $request->telefone;
-            $user->imagem = $request->imagem;
+            $user->nome = $google_user->name;
+            $user->password = bcrypt($request->token);
+            $user->email = $google_user->email;
+            $user->imagem = $google_user->avatar;
             $user->is_google = true;
             $user->save();
+
         }
         $token = auth('api')->login($user);
 
@@ -86,8 +90,12 @@ class AuthController extends Controller {
         //TODO tratar se email existe
         $credentials = $request->validate(['email' => 'required|email']);
         $user = User::where('email', $request->only('email'))->firstOrFail();
-        Password::sendResetLink($credentials);
-        return response()->json(["msg" => 'Reset password link sent on your email id.']);
+        if ($user != null) {
+            Password::sendResetLink($credentials);
+            return response()->json(["msg" => 'Reset password link sent on your email id.']);
+        } else {
+            return response()->json(["msg" => "Usuário não encontrado"], 401);
+        }
     }
 
     public function reset() {
