3f0c89026fcd287b5fdead40d99d340c60fbda29
Nikollas <nikollasferreirag@gmail.com>
2021-04-14 15:54:57 -0300

    feat: configura lib socialite

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Laravel\Socialite\SocialiteServiceProvider::class,
     ],
 
     /*
@@ -231,6 +231,7 @@ return [
         'OneSignal' => Berkayk\OneSignal\OneSignalFacade::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
     ],
 
 ];
