0e0856c98bd7f4a6aaf86e2324cb94870afa3d61
Nikollas <nikollasferreirag@gmail.com>
2021-04-28 09:55:53 -0300

    feat: remove rota de welcome

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('email/verify/{id}', 'VerificationController@verify')->name('verification.verify'); // Make sure to keep this as your route name
+Route::get('email/verify/{id}', 'VerificationController@verify')->name('verification.verify');
 
 Route::get('email/resend', 'VerificationController@resend')->name('verification.resend');
 Route::view('forgot_password', 'auth.reset_password')->name('password.reset');
